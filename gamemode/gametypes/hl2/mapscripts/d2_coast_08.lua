c55ee16ccf0a4f355145c8141d890a5a4bafe72a
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2025-02-08 22:02:28 +0200

    Improve d2_coast_07 and d2_coast_08

--- gamemode/gametypes/hl2/mapscripts/d2_coast_08.lua
+++ gamemode/gametypes/hl2/mapscripts/d2_coast_08.lua
@@ -32,22 +32,90 @@ MAPSCRIPT.VehicleGuns = true
 
 function MAPSCRIPT:PostInit()
     if SERVER then
-        -- 3233.962402 -5601.413086 1564.521851
-        local checkpoint0 = GAMEMODE:CreateCheckpoint(Vector(3332.003174, 1683.768311, 1536.031250), Angle(0, -90, 0))
-        GAMEMODE:SetPlayerCheckpoint(checkpoint0)
-        local checkpoint4 = GAMEMODE:CreateCheckpoint(Vector(3325.756836, -5639.022461, 1536.031250), Angle(0, 0, 0))
-        local checkpointTrigger4 = ents.Create("trigger_multiple")
-        checkpointTrigger4:SetupTrigger(Vector(3302.523193, -5592.021484, 1536.031250), Angle(0, 0, 0), Vector(-250, -105, 0), Vector(250, 105, 200))
+        ents.RemoveByClass("info_player_start")
 
-        checkpointTrigger4.OnTrigger = function(trigger, activator)
-            GAMEMODE:SetPlayerCheckpoint(checkpoint4, activator)
-            trigger:Disable()
+        local newPlayerSpawn = ents.Create("info_player_start")
+        newPlayerSpawn:SetPos(Vector(3328.9, 5241.59, 1536.1))
+        newPlayerSpawn:SetAngles(Angle(0, -90, 0))
+        newPlayerSpawn:SetKeyValue("spawnflagas", "1") -- master
+        newPlayerSpawn:Spawn()
+
+        local cp1 = GAMEMODE:CreateCheckpoint(Vector(3122.5, 988.9, 1480.1), Angle(0, -90, 0))
+        cp1:SetRenderPos(Vector(3343.7, 956.5, 1480.1))
+        local cp1Trigger = ents.Create("trigger_multiple")
+        cp1Trigger:SetupTrigger(
+            Vector(3343.7, 956.5, 1480.1),
+            Angle(0, 0, 0),
+            Vector(-250, -60, 0),
+            Vector(250, 60, 580)
+        )
+        cp1Trigger.OnTrigger = function(_, activator)
+            cp1Trigger:Input("Disable")
+            GAMEMODE:SetPlayerCheckpoint(cp1, activator)
+        end
+
+        local cp2 = GAMEMODE:CreateCheckpoint(Vector(3257, -2004, 1551.6), Angle(0, -90, 0))
+        local cp2Trigger = ents.Create("trigger_once")
+        cp2Trigger:SetupTrigger(
+            Vector(3257, -2004, 1551.6),
+            Angle(0, 0, 0),
+            Vector(-70, -60, 0),
+            Vector(70, 60, 80)
+        )
+        cp2Trigger.OnTrigger = function(_, activator)
+            GAMEMODE:SetPlayerCheckpoint(cp2, activator)
+        end
+
+        local cp4 = GAMEMODE:CreateCheckpoint(Vector(3331.41, -2021.9, 1792.1), Angle(0, 90, 0))
+        cp4:AddEffects(EF_NODRAW)
+        local cp4Trigger = ents.Create("trigger_once")
+        cp4Trigger:SetupTrigger(
+            Vector(3331.41, -2021.9, 1792.1),
+            Angle(0, 0, 0),
+            Vector(-70, -70, 0),
+            Vector(70, 70, 80)
+        )
+        cp4Trigger:KeyValue("StartDisabled", "1")
+        cp4Trigger.OnTrigger = function(_, activator)
+            GAMEMODE:SetPlayerCheckpoint(cp4, activator)
+            if IsValid(cp1) then
+                cp1:Reset()
+            end
+            if IsValid(cp1Trigger) then
+                cp1Trigger:Fire("Enable")
+            end
+        end
+
+        local cp3 = GAMEMODE:CreateCheckpoint(Vector(2849.5, -3522.9, 1920.1), Angle(0, 0, 0))
+        cp3:AddEffects(EF_NODRAW)
+        local cp3Trigger = ents.Create("trigger_once")
+        cp3Trigger:SetupTrigger(
+            Vector(2849.5, -3522.9, 1920.1),
+            Angle(0, 0, 0),
+            Vector(-30, -30, 0),
+            Vector(230, 30, 80)
+        )
+        cp3Trigger.OnTrigger = function(_, activator)
+            GAMEMODE:SetPlayerCheckpoint(cp3, activator)
+            if IsValid(cp4Trigger) then
+                cp4:RemoveEffects(EF_NODRAW)
+                cp4Trigger:Fire("Enable")
+            end
         end
 
         GAMEMODE:WaitForInput("button_trigger", "Use", function()
-            checkpoint4:Reset()
-            checkpointTrigger4:Enable()
             GAMEMODE:EnablePreviousMap()
+        end)
+
+        -- Resize the trigger_transition volume, its too small so the gunship can sometimes
+        -- get lost when its not killed.
+        ents.WaitForEntityByName("landmark_d2_coast_07-08", function(ent)
+            if ent:GetClass() == "trigger_transition" then
+                ent:ResizeTriggerBox(
+                    Vector(-1060, -560, 0), 
+                    Vector(6560, 8560, 3060)
+                )
+            end
         end)
     end
 end
